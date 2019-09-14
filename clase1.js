2b94a98f7311799459fa1e967c6039074b6ebc71
Fer Torres <fertorresmx@gmail.com>
2019-09-14 13:46:41 -0500

    Usando substr

--- clase1.js
+++ clase1.js
@@ -7,3 +7,5 @@ var primeraLetraDelNombre = nombre.charAt(0)
 var cantidadDeLetrasDelNombre = nombre.length
 
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}` 
+
+var str = nombre.substr(1, 2)
