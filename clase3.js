3b355d9874d8a514130bf9e6dadc817ab0165d9e
Fer Torres <fertorresmx@gmail.com>
2019-09-14 14:05:27 -0500

    Incrementando/Decrementando valores a variables

--- clase3.js
+++ clase3.js
@@ -2,3 +2,15 @@ var edad = 27
 
 //edad = edad + 1 
 edad += 1
+
+var peso = 75
+//peso = peso - 2
+peso -= 2
+
+var sandwich = 1
+//peso = peso + sandwich
+peso += sandwich
+
+var jugarAlFutbol = 3
+//peso = peso - jugarAlFutbol
+peso -= jugarAlFutbol
