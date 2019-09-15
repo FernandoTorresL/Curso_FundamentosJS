b2b54f8ba7fc7b8dff072aa07d50cdddb1f10685
Fer Torres <fertorresmx@gmail.com>
2019-09-14 23:31:15 -0500

    Usando funciones v3

--- clase5.js
+++ clase5.js
@@ -1,8 +1,18 @@
-var nombre = 'Fer'
-
-function imprimirNombreEnMayusculas(n) {
-    n = n.toUpperCase()
-    console.log(n)
+var fer = {
+    nombre: 'FerT',
+    apellido: 'Torres',
+    edad: 28
 }
 
-imprimirNombreEnMayusculas(nombre)
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 22
+}
+
+function imprimirNombreEnMayusculas(persona) {
+    console.log(persona.nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(fer)
+imprimirNombreEnMayusculas(dario)
