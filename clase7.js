796e711fd0ace036e03f7702c1e970bcda4c96d9
Fer Torres <fertorresmx@gmail.com>
2019-09-16 16:48:21 -0500

    Solución al reto 'Destructurar objetos'

---
+++ clase7.js
@@ -0,0 +1,19 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    edad: 28
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Susnisky',
+    edad: 27
+}
+
+function imprimirNombreyEdad(persona) {
+    console.log(`Hola, me llamo ${persona.nombre} y tengo ${persona.edad} años.`)
+}
+
+console.log('Impresión de resultados del reto en clase Destructurar objetos:')
+imprimirNombreyEdad(sacha)
+imprimirNombreyEdad(dario)
