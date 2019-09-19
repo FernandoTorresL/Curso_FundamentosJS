50f32e4dd58a57e945cb944eafa4dc8ca3721489
Fernando Torres <fertorresmx@gmail.com>
2019-09-18 22:54:33 -0500

    Clase 21 - Solución al reto

--- clase21.js
+++ clase21.js
@@ -1,17 +1,27 @@
 // Prototipo de Persona
-function Persona(nombre, apellido) {
+function Persona(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
+    this.altura = altura
 }
 
 Persona.prototype.saludar = function () {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}. ${this.soyAlto()}`)
 }
 
-// Crear objeto
-var sacha = new Persona('Sacha', 'Lifszyc')
-var erika = new Persona('Erika', 'Luna')
-var arturo = new Persona('Arturo', 'Martínez')
+Persona.prototype.soyAlto = function () {
+    if (this.altura > 1.8) 
+        msg = 'Soy una persona alta!'
+    else
+        msg = 'Soy una persona baja!'
 
-// Método
+    return msg
+}
+
+var sacha = new Persona('Sacha', 'Lifszyc', 1.9)
+var erika = new Persona('Erika', 'Luna', 1.6)
+var arturo = new Persona('Arturo', 'Martínez', 1.8)
+
 sacha.saludar()
+erika.saludar()
+arturo.saludar()
